fed9827351206d1df0d2d8239edddc5065f9da28
bangshiuh <chen1671@purdue.edu>
2017-09-19 10:02:52 -0400

    [Transport] init of IonGasTransport

---
+++ include/cantera/transport/IonGasTransport.h
@@ -0,0 +1,13 @@
+/**
+ * @file GasTransport.h
+ */
+
+// This file is part of Cantera. See License.txt in the top-level directory or
+// at http://www.cantera.org/license.txt for license and copyright information.
+
+#ifndef CT_ION_GAS_TRANSPORT_H
+#define CT_ION_GAS_TRANSPORT_H
+
+#include "GasTransport.h"
+
+#endif
---
+++ src/transport/IonGasTransport.cpp
@@ -0,0 +1,11 @@
+//! @file IonGasTransport.cpp
+
+// This file is part of Cantera. See License.txt in the top-level directory or
+// at http://www.cantera.org/license.txt for license and copyright information.
+
+#include "cantera/transport/IonGasTransport.h"
+
+namespace Cantera
+{
+    
+}
