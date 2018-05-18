ac1d2e733d28fd0cfb0e018472998f6e53d85f31
bangshiuh <chen1671@purdue.edu>
2018-05-18 16:01:19 -0400

    [Transport] Add use Blancs law to calculate mobilities

--- include/cantera/transport/IonGasTransport.h
+++ include/cantera/transport/IonGasTransport.h
@@ -26,14 +26,21 @@ public:
     virtual void init(thermo_t* thermo, int mode, int log_level);
     virtual double viscosity();
     virtual double thermalConductivity();
+    //! The ion mobilities are calculated by Blanc's law
+    /*! Chiflikian, R. V. "The analog of Blanc’s law for drift velocities
+     *  of electrons in gas mixtures in weakly ionized plasma."
+     *  Physics of Plasmas 2.10 (1995): 3902-3909.
+     */
+    virtual void getMobilities(double* const mobi);
+    //! The mixture transport for ionized gas
+    //! The binary transport between two charged species is neglected.
+    virtual void getMixDiffCoeffs(double* const d);
 
 protected:
     //! setup parameters for n64 model
     void setupN64();
     virtual void fitDiffCoeffs(MMCollisionInt& integrals);
     double omega11_n64(const double tstar, const double gamma);
-
-    virtual void getMixDiffCoeffs(doublereal* const d);
 
     //! electrical properties
     vector_int m_speciesCharge;
