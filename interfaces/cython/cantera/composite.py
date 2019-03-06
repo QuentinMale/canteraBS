f27232fda970a1e50d7a07c1b663a3af1f42dcd0
bangshiuh <bangshiuh.chen@gmail.com>
2019-03-06 12:15:50 +0800

    enable reading and storing data of electron

--- interfaces/cython/cantera/composite.py
+++ interfaces/cython/cantera/composite.py
@@ -85,6 +85,10 @@ class Solution(ThermoPhase, Kinetics, Transport):
     __slots__ = ()
 
 
+class Plasma(ThermoPhase, Kinetics, Transport, Electron):
+    __slots__ = ()
+
+
 class Interface(InterfacePhase, InterfaceKinetics):
     """
     Two-dimensional interfaces.
