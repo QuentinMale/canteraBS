3d71a1269f295aae47eb26163f4dd7c78c61356c
bangshiuh <bangshiuh.chen@gmail.com>
2020-04-20 13:59:56 -0400

    [test] add test_plasma to test_kinetics.py

--- interfaces/cython/cantera/composite.py
+++ interfaces/cython/cantera/composite.py
@@ -85,6 +85,10 @@ class Solution(ThermoPhase, Kinetics, Transport):
     __slots__ = ()
 
 
+class Plasma(PlasmaPhase, Kinetics, Transport):
+    __slots__ = ()
+
+
 class Interface(InterfacePhase, InterfaceKinetics):
     """
     Two-dimensional interfaces.
