7e54857e3f5a363ecaa1ff05b363c91ba25bf1f3
bangshiuh <bangshiuh.chen@gmail.com>
2022-05-02 15:56:10 -0400

    [interface, reaction] add ElectronCollisionPlasmaRate

---
+++ include/cantera/kinetics/ElectronCollisionPlasmaRate.h
@@ -0,0 +1,110 @@
+//! @file ElectronCollisionPlasmaRate.h   Header for plasma reaction rates parameterized
+//!     by electron collision cross section and electron energy distribution.
+
+// This file is part of Cantera. See License.txt in the top-level directory or
+// at https://cantera.org/license.txt for license and copyright information.
+
+#ifndef CT_ELECTRONCOLLISIONPLASMARATE_H
+#define CT_ELECTRONCOLLISIONPLASMARATE_H
+
+#include "cantera/thermo/PlasmaPhase.h"
+#include "cantera/kinetics/ReactionData.h"
+#include "ReactionRate.h"
+#include "MultiRate.h"
+
+namespace Cantera
+{
+
+//! Data container holding shared data specific to ElectronCollisionPlasmaRate
+/**
+ * The data container `ElectronCollisionPlasmaData` holds precalculated data common to
+ * all `ElectronCollisionPlasmaRate` objects.
+ */
+struct ElectronCollisionPlasmaData : public ReactionData
+{
+    ElectronCollisionPlasmaData();
+
+    virtual bool update(const ThermoPhase& phase, const Kinetics& kin) override;
+    using ReactionData::update;
+
+    virtual void invalidateCache() override {
+        ReactionData::invalidateCache();
+        EnergyLevels.resize(0);
+        Distribution.resize(0);
+    }
+
+    Eigen::ArrayXd EnergyLevels; //!< electron energy levels
+    Eigen::ArrayXd Distribution; //!< electron energy distribution
+};
+
+
+//! Electron collision plasma reaction rate type
+/*!
+ * The electron collision plasma reaction rate uses the electron collision
+ * data and the electron energy distribution to calculate the reaction rate.
+ *
+ *   @f[
+ *        k_f =  \gamma \int_0^{\infty} \epsilon \sigma F_0 d\epsilon
+ *   @f]
+ *
+ * where @f$ \gamma = (2e/m)^{1/2} @f$ is a constant, @f$ \epsilon @f$ is
+ * the electron energy, @f$ \sigma @f$ is the reaction collision cross
+ * section, and @f$ F_0 @f$ is the normalized electron energy distribution
+ * function.
+ *
+ * References:
+ *
+ * [1] Hagelaar and Pitchford @cite hagelaar2005.
+ *
+ * @ingroup plasma
+ */
+class ElectronCollisionPlasmaRate : public ReactionRate
+{
+public:
+    ElectronCollisionPlasmaRate();
+
+    ElectronCollisionPlasmaRate(const AnyMap& node,
+                                const UnitStack& rate_units={})
+        : ElectronCollisionPlasmaRate()
+    {
+        setParameters(node, rate_units);
+    }
+
+    virtual void setParameters(const AnyMap& node, const UnitStack& units) override;
+
+    virtual void getParameters(AnyMap& node) const override;
+
+    unique_ptr<MultiRateBase> newMultiRate() const override {
+        return unique_ptr<MultiRateBase>(
+            new MultiRate<ElectronCollisionPlasmaRate, ElectronCollisionPlasmaData>);
+    }
+
+    virtual const std::string type() const override {
+        return "electron-collision-plasma";
+    }
+
+    virtual void setContext(const Reaction& rxn, const Kinetics& kin) override;
+
+    //! Evaluate reaction rate
+    /*!
+     *  @param shared_data  data shared by all reactions of a given type
+     */
+    double evalFromStruct(const ElectronCollisionPlasmaData& shared_data) const;
+
+    //! Evaluate derivative of reaction rate with respect to temperature
+    //! divided by reaction rate
+    //! @param shared_data  data shared by all reactions of a given type
+    double ddTScaledFromStruct(const ElectronCollisionPlasmaData& shared_data) const {
+        throw NotImplementedError("ElectronCollisionPlasmaRate<>::ddTScaledFromStruct");
+    }
+
+    //! electron energy levels
+    Eigen::ArrayXd m_energyLevels;
+
+    //! collision crosss section
+    Eigen::ArrayXd m_crossSection;
+};
+
+}
+
+#endif
---
+++ src/kinetics/ElectronCollisionPlasmaRate.cpp
@@ -0,0 +1,121 @@
+//! @file ElectronCollisionPlasmaRate.cpp
+
+// This file is part of Cantera. See License.txt in the top-level directory or
+// at https://cantera.org/license.txt for license and copyright information.
+
+#include "cantera/kinetics/ElectronCollisionPlasmaRate.h"
+#include "cantera/kinetics/Reaction.h"
+#include "cantera/kinetics/Kinetics.h"
+#include "cantera/numerics/funcs.h"
+
+namespace Cantera
+{
+
+ElectronCollisionPlasmaData::ElectronCollisionPlasmaData()
+{
+    EnergyLevels.resize(1);
+    EnergyLevels << 0.0;
+    Distribution.resize(1);
+    Distribution << 0.0;
+}
+
+bool ElectronCollisionPlasmaData::update(const ThermoPhase& phase,
+                                         const Kinetics& kin)
+{
+    const PlasmaPhase& pp = dynamic_cast<const PlasmaPhase&>(phase);
+
+    Eigen::ArrayXd levels(pp.nElectronEnergyLevels());
+    pp.getElectronEnergyLevels(levels.data());
+
+    Eigen::ArrayXd dist(pp.nElectronEnergyLevels());
+    pp.getElectronEnergyDistribution(dist.data());
+
+    bool changed = false;
+
+    if (levels.size() != EnergyLevels.size() || (levels - EnergyLevels).any() != 0.0) {
+        EnergyLevels = std::move(levels);
+        changed = true;
+    }
+
+    if (dist.size() != Distribution.size() || (dist - Distribution).any() != 0.0) {
+        Distribution = std::move(dist);
+        changed = true;
+    }
+
+    return changed;
+}
+
+ElectronCollisionPlasmaRate::ElectronCollisionPlasmaRate()
+{
+}
+
+void ElectronCollisionPlasmaRate::setParameters(const AnyMap& node, const UnitStack& rate_units)
+{
+    ReactionRate::setParameters(node, rate_units);
+    if (node.hasKey("energy-levels")) {
+        vector<double> levels = node["energy-levels"].asVector<double>();
+        m_energyLevels = Eigen::Map<const Eigen::ArrayXd>(levels.data(), levels.size());
+    } else {
+        m_energyLevels = NAN;
+    }
+    if (node.hasKey("cross-section")) {
+        vector<double> crossSection = node["cross-section"].asVector<double>();
+        m_crossSection = Eigen::Map<const Eigen::ArrayXd>(crossSection.data(), crossSection.size());
+    } else {
+        m_crossSection = NAN;
+    }
+    if (m_energyLevels.size() != m_crossSection.size()) {
+        throw CanteraError("ElectronCollisionPlasmaRate::setParameters",
+            "Energy levels and cross section must have the same length.");
+    }
+}
+
+void ElectronCollisionPlasmaRate::getParameters(AnyMap& node) const {
+    node["type"] = type();
+    vector<double> energyLevels(m_energyLevels.size());
+    Eigen::Map<Eigen::ArrayXd>(energyLevels.data(), energyLevels.size()) = m_energyLevels;
+    node["energy-levels"] = energyLevels;
+    vector<double> crossSection(m_crossSection.size());
+    Eigen::Map<Eigen::ArrayXd>(crossSection.data(), crossSection.size()) = m_crossSection;
+    node["cross-section"] = crossSection;
+}
+
+double ElectronCollisionPlasmaRate::evalFromStruct(const ElectronCollisionPlasmaData& shared_data) const
+{
+    Eigen::ArrayXd eps2 = shared_data.EnergyLevels.pow(2.0);
+    Eigen::ArrayXd cross_section(shared_data.EnergyLevels.size());
+    linearInterp(shared_data.EnergyLevels, m_energyLevels, m_crossSection, cross_section);
+    return 0.5 * pow(2.0 * ElectronCharge / ElectronMass, 0.5) *
+           simpson(shared_data.Distribution.cwiseProduct(cross_section), eps2);
+}
+
+void ElectronCollisionPlasmaRate::setContext(const Reaction& rxn, const Kinetics& kin)
+{
+    // ElectronCollisionPlasmaReaction is for a non-equilibrium plasma, and the reverse rate
+    // cannot be calculated from the conventional thermochemistry.
+    // @todo implement the reversible rate for non-equilibrium plasma
+    if (rxn.reversible) {
+        throw InputFileError("ElectronCollisionPlasmaRate::setContext", rxn.input,
+            "ElectronCollisionPlasmaRate does not support reversible reactions");
+    }
+    if (rxn.reactants.size() != 2) {
+        throw InputFileError("ElectronCollisionPlasmaRate::setContext", rxn.input,
+            "ElectronCollisionPlasmaRate requires exactly two reactants");
+    }
+    // get electron species name
+    string electronName;
+    if (kin.thermo().type() == "plasma") {
+        electronName = dynamic_cast<const PlasmaPhase&>(kin.thermo()).electronSpeciesName();
+    } else {
+        throw CanteraError("ElectronCollisionPlasmaRate::setContext",
+                           "ElectronCollisionPlasmaRate requires plasma phase");
+    }
+    // find electron in reactants
+    if (rxn.reactants.find(electronName) == rxn.reactants.end()) {
+        throw InputFileError("ElectronCollisionPlasmaRate::setContext", rxn.input,
+            "ElectronCollisionPlasmaRate requires at least an electron species as "
+            "one reactant");
+    }
+}
+
+}
